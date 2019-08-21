d978e3a52d723e9da9ba4cbb787f14c5a3417a15
Jean Lauliac <jean@lauliac.com>
2019-08-21 13:08:36 +0100

    improve refinement data structure with variant-specific fields

--- clover_comp.js
+++ clover_comp.js
@@ -390,7 +390,7 @@ function analyse_statement(state, statement, scope, refims) {
 
 }
 
-const EMPTY_MAP = {};
+const EMPTY_MAP = new Map();
 
 function analyse_expression(state, exp, scope, refims) {
   if (exp.__type === 'Bool_literal') {
@@ -448,14 +448,27 @@ function analyse_expression(state, exp, scope, refims) {
     const {reference, refinements} = operand;
     invariant(reference != null);
     const {path} = reference;
-    let target = {variant_ids: [variant_id]};
+    let target = {
+      __type: 'Enum_refinement',
+      fields_by_variant_id: new Map([[variant_id, EMPTY_MAP]]),
+    };
     for (let i = path.length - 1; i >= 0; --i) {
       const item = path[i];
-      invariant(item.__type === 'Field');
-      target = {fields: new Map([[item.name, target]])};
+      const fields = new Map([[item.name, target]]);
+      if (item.__type === 'Struct_field_access') {
+        target = {__type: 'Struct_refinement', fields};
+        continue;
+      }
+      invariant(item.__type === 'Enum_field_access');
+      target = {
+        __type: 'Enum_refinement',
+        fields_by_variant_id: new Map([[item.variant_id, fields]]),
+      };
     }
     const conditional_refinements = new Map();
     conditional_refinements.set(reference.value_id, target);
+
+    // console.error(require('util').inspect(conditional_refinements, {depth: null}));
 
     return {type: state.builtins.bool, conditional_refinements, refinements};
   }
@@ -679,46 +692,62 @@ function merge_refinements(method, refims, right_refims) {
 
 function merge_refinement_entry(method, entry, right_entry) {
   invariant(method === 'Intersection' || method === 'Union');
+  invariant(entry.__type != null);
+  invariant(right_entry.__type != null);
 
-  let variants_ids;
-
-  if (entry.variant_ids != null && right_entry.variant_ids == null) {
-    if (method === 'Intersection') ({variant_ids} = entry);
-  } else if (entry.variant_ids == null && right_entry.variant_ids != null) {
-    if (method === 'Intersection') ({variant_ids} = right_entry);
-  } else if (entry.variant_ids != null && right_entry.variant_ids != null) {
-    const possible_ids = new Set(entry.variant_ids);
-    if (method === 'Union') {
-      for (const id of right_entry.variant_ids) {
-        possible_ids.add(id);
-      }
-      variant_ids = Array.from(possible_ids);
-    } else {
-      variant_ids = [];
-      for (const id of right_entry.variant_ids) {
-        if (possible_ids.has(id)) {
-          variant_ids.push(id);
-        }
-      }
-      invariant(variant_ids.length > 0);
-    }
+  if (entry.__type === 'Struct_refinement') {
+    invariant(right_entry.__type === 'Struct_refinement');
+    return {
+      __type: 'Struct_refinement',
+      fields: merge_refinement_fields(method, entry.field, right_entry.fields),
+    };
   }
 
+  invariant(entry.__type === 'Enum_refinement');
+  invariant(right_entry.__type === 'Enum_refinement');
+
+  let fields_by_variant_id = new Map();
+
+  for (const [id, fields] of entry.fields_by_variant_id.entries()) {
+    const right_fields = right_entry.fields_by_variant_id.get(id);
+    if (right_fields == null) {
+      if (method === 'Union') fields_by_variant_id.set(id, fields);
+      continue;
+    }
+    fields_by_variant_id.set(id,
+        merge_refinement_fields(method, fields, right_field));
+  }
+
+  if (method === 'Intersection')
+    return {__type: 'Enum_refinement', fields_by_variant_id};
+
+  for (const [id, right_fields] of right_entry.fields_by_variant_id.entries()) {
+    if (entry.fields_by_variant_id.has(id)) continue;
+    fields_by_variant_id.set(id, right_fields);
+  }
+  return {__type: 'Enum_refinement', fields_by_variant_id};
+}
+
+function merge_refinement_fields(method, fields, right_fields) {
+  invariant(method === 'Intersection' || method === 'Union');
+
   const fields = new Map();
-  for (const [name, field] of entry.fields.entries()) {
-    const right_field = right_entry.get(name);
+  for (const [name, field] of fields.entries()) {
+    const right_field = right_fields.get(name);
     if (right_field == null) {
       fields.set(name, field);
       continue;
     }
-    fields.set(value_id, merge_refinement_entry(method, field, right_field));
+    const new_field = merge_refinement_entry(method, field, right_field);
+    if (new_field != null)
+      fields.set(value_id, new_field);
   }
-  for (const [name, right_field] of right_refims.fields.entries()) {
+  for (const [name, right_field] of right_fields.entries()) {
     if (fields.has(name)) continue;
     fields.set(name, right_field);
   }
 
-  return {variant_ids, fields};
+  return fields;
 }
 
 function resolve_type(state, scope, type) {
@@ -761,21 +790,26 @@ function resolve_qualified_name(state, scope, name, refims) {
       const field_spec = type_spec.fields.get(field_name);
       if (field_spec == null) throw new Error(`cannot find field "${field_name}"`);
       ({type} = field_spec);
-      refim = refim && refim.fields && refim.fields.get(field_name);
-      path.push({__type: 'Field', name: field_name});
+      if (refim != null) {
+        invariant(refim.__type === 'Struct_refinement');
+        refim = refim.fields.get(field_name);
+      }
+      path.push({__type: 'Struct_field_access', name: field_name});
       continue;
     }
 
-    if (type_spec.__type === 'Enum' && refim && refim.variant_ids != null) {
-      invariant(refim.variant_ids.length === 1);
-      const variant_spec = state.types.get(refim.variant_ids[0]);
+    if (type_spec.__type === 'Enum' && refim != null) {
+      invariant(refim.__type === 'Enum_refinement');
+      invariant(refim.fields_by_variant_id.size === 1);
+      const variant_id = refim.fields_by_variant_id.keys().next().value;
+      const variant_spec = state.types.get(variant_id);
       invariant(variant_spec.__type === 'Enum_variant');
 
       const field_spec = variant_spec.fields.get(field_name);
       if (field_spec == null) throw new Error(`cannot find field "${field_name}"`);
       ({type} = field_spec);
-      refim = refim && refim.fields && refim.fields.get(field_name);
-      path.push({__type: 'Field', name: field_name});
+      refim = refim.fields_by_variant_id.get(variant_id);
+      path.push({__type: 'Enum_field_access', variant_id, name: field_name});
       continue;
     }
 
