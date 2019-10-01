3d452d6a4184d8d3ca7c324a61a402a6bc8a5c56
Jean Lauliac <jean@lauliac.com>
2019-10-01 17:17:13 +0100

    Extract build() function

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 
 ./cli.js > dist/compiled_src.js
 cp ./cli.js dist/
+cp ./build.js dist/
 dist/cli.js > dist/compiled_src.new.js
 cp dist/compiled_src.new.js dist/compiled_src.js
 dist/cli.js > dist/compiled_src.new.js
